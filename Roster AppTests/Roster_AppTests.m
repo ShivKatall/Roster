56b8e0fc4c956778b3cf846b3dae6ce79842b8d0
Cole Bratcher <colebratcher@gmail.com>
2014-04-10 21:27:43 -0700

    Commit of files after computer issues on 04/10/14

---
+++ Roster AppTests/Roster_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Roster_AppTests.m
+//  Roster AppTests
+//
+//  Created by Cole Bratcher on 4/7/14.
+//  Copyright (c) 2014 Cole Bratcher. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Roster_AppTests : XCTestCase
+
+@end
+
+@implementation Roster_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
